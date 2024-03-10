a45ea078f87a17e17d69fbfee8382a200800980b
hydroshiba <36961737+hydroshiba@users.noreply.github.com>
2024-03-11 02:17:05 +0700

    Fix some bugs & modify version 2
    The more you play it the stronger it is

--- source/bot/v2.cpp
+++ source/bot/v2.cpp
@@ -1,4 +1,5 @@
 #include "v2.hpp"
+#include <iostream>
 
 v2::v2() {
     ran.seed(std::random_device()());
@@ -6,15 +7,16 @@ v2::v2() {
 	factorial[0] = 1;
 	for(int i = 1; i < 10; ++i) factorial[i] = i * factorial[i - 1];
 
-	weight.resize(factorial[9]);
-	for(int i = 0; i < factorial[9]; ++i) order.push_back(i);
+	weight.resize(factorial[9], 1);
+	std::vector<int> perms(factorial[9]);
+	
+	std::iota(perms.begin(), perms.end(), 0);
+	std::shuffle(perms.begin(), perms.end(), ran);
 
-	// Default configuration
-	--weight[index({7, 5, 1, 3, 6, 2, 8, 0, 4})];
-	reorder();
+	for(int i : perms) order.insert({weight[i], i});
 }
 
-int v2::index(std::vector<int> perm) {
+int v2::index(std::vector<int> perm) const {
 	int ans = 0;
 
 	for(int i = 0; i < 9; ++i) {
@@ -27,7 +29,7 @@ int v2::index(std::vector<int> perm) {
 	return ans;
 }
 
-std::vector<int> v2::permutation(int index) {
+std::vector<int> v2::permutation(int index) const {
 	std::vector<int> perm(9);
 	for(int i = 0; i < 9; ++i) perm[i] = i;
 
@@ -40,27 +42,22 @@ std::vector<int> v2::permutation(int index) {
 	return perm;
 }
 
-void v2::reorder() {
-	std::shuffle(order.begin(), order.end(), ran);
-	std::sort(order.begin(), order.end(), [&](int a, int b) {
-		return weight[a] < weight[b];
-	});
-}
-
 byte v2::move(Player turn, char board[3][3]) {
 	int mask = this->mask(board);
 	byte last, cell = 9, blocks = 0;
 	
-	std::vector<int> cells = permutation(order[0]);
+	std::vector<int> cells = permutation(order.begin()->second);
+	order.erase(order.begin()); // Pop permutation with least weight from the set
 
 	for(int i = 0; i < 9; ++i) {
 		if(!get(mask, cells[i])) {
 			// Winning move
 			set(mask, cells[i], turn);
 			if(win(mask)) {
-				--weight[index(cells)]; // Winning, decrease weight to current permutation
-				reorder();
-				return cells[i]; // Return immediately
+				// Winning, half the weight to current permutation
+				weight[index(cells)] = std::max(1, weight[index(cells)] >> 1);
+				order.insert({weight[index(cells)], index(cells)});
+				return cells[i];
 			}
 
 			// Blocking move
@@ -72,12 +69,10 @@ byte v2::move(Player turn, char board[3][3]) {
 		}
 	}
 
-	// Losing, increase weight to current permutation
-	if(blocks > 1) {
-		++weight[index(cells)];
-		reorder();
-	}
-
 	if(cell == 9) cell = last; // If no critical move found, return last empty cell in the list
+	if(blocks > 1) weight[index(cells)] <<= 1; // Losing, double the weight to current permutation
+	else weight[index(cells)] = std::max(1, weight[index(cells)] >> 1); // Boring, decrease weight by 1 to current permutation
+
+	order.insert({weight[index(cells)], index(cells)}); // Re-add permutation to the queue
 	return cell;
 }
--- source/bot/v2.hpp
+++ source/bot/v2.hpp
@@ -4,20 +4,21 @@
 #include <algorithm>
 #include <vector>
 #include <random>
+#include <set>
+#include <utility>
 
 #include "bot.hpp"
 
 class v2: public Bot {
 private:
 	std::mt19937 ran;
-
+	
 	int factorial[10];
-	std::vector<int> order, weight;
-
-	int index(std::vector<int> perm);
-	std::vector<int> permutation(int index);
-
-	void reorder();
+	std::vector<int> weight;
+	std::set<std::pair<int, int>> order;
+	
+	int index(std::vector<int> perm) const;
+	std::vector<int> permutation(int index) const;
 
 public:
 	v2();
